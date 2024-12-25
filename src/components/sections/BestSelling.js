17a685ce0fd8f3bdcaa1c9fd0420f470aeb2058f
shafayet <shafayetsec09@gmail.com>
2024-12-26 01:33:22 +0600

    home page card show more in one row and space reduce

--- src/components/sections/BestSelling.js
+++ src/components/sections/BestSelling.js
@@ -40,7 +40,7 @@ const BestSelling = () => {
         <span>Not found</span>
       </div>:
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
--- src/components/sections/FeaturedProducts.js
+++ src/components/sections/FeaturedProducts.js
@@ -41,7 +41,7 @@ const FeaturedProducts = () => {
         <span>Not found</span>
       </div>:
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
