80a157db1e4ec5283e1c9dcb52cfd701e4846fe1
shafayet <shafayetsec09@gmail.com>
2025-02-07 16:42:43 +0600

    clear cart after logut

--- src/redux/slice/authActions.js
+++ src/redux/slice/authActions.js
@@ -2,8 +2,10 @@
 import { persistStore } from 'redux-persist';
 import store from '../store';
 import {logoutUser} from './authSlice';
+import {clearCart} from './cartSlice';
 
 export const Logout = (logout_data) => (dispatch) => {
   dispatch(logoutUser(logout_data)); // Reset authentication state
+  dispatch(clearCart());
   persistStore(store).purge(); // Clear persisted state
 };
