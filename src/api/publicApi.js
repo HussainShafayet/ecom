f54c6537cba76933e6859c30c8bd83989dd07075
shafayet <shafayetsec09@gmail.com>
2025-02-25 02:51:03 +0600

    global error added in public api setup

--- src/api/publicApi.js
+++ src/api/publicApi.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {setGlobalError} from "../redux/slice/globalErrorSlice";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
@@ -9,5 +10,32 @@ const publicApi = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// Response Interceptor for Global Error Handling
+publicApi.interceptors.response.use(
+  (response) => response, // Return response if no errors
+  (error) => {
+    import("../redux/store").then(({ default: store }) => {
+      if (!error.response) {
+        store.dispatch(setGlobalError("Network error: Unable to connect to the server"));
+      } else if (error.response.status === 404) {
+        store.dispatch(setGlobalError("Requested resource not found"));
+      } else if (error.response.status >= 500) {
+        store.dispatch(setGlobalError("Server error: Please try again later"));
+      } else if (error.response.status === 400) {
+        store.dispatch(setGlobalError("Invalid request. Please check your input."));
+      } else if (error.response.status === 403) {
+        store.dispatch(setGlobalError("You do not have permission to access this resource."));
+      } else if (error.response.status === 429) {
+        store.dispatch(setGlobalError("Too many requests. Please try again later."));
+      } else {
+        store.dispatch(setGlobalError("An unexpected error occurred. Please try again."));
+      }
+    });
+
+    return Promise.reject(error);
+  }
+);
+
 
 export default publicApi;
