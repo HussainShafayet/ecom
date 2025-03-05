24089c5ffe4d20c656981406279dae1d83db7e11
shafayet <shafayetsec09@gmail.com>
2025-03-06 01:28:28 +0600

    after logout e clear wish list

--- src/redux/slice/authActions.js
+++ src/redux/slice/authActions.js
@@ -3,9 +3,11 @@ import { persistStore } from 'redux-persist';
 import store from '../store';
 import {logoutUser} from './authSlice';
 import {clearCart} from './cartSlice';
+import {clearWishlist} from './wishlistSlice';
 
 export const Logout = (logout_data) => (dispatch) => {
   dispatch(logoutUser(logout_data)); // Reset authentication state
   dispatch(clearCart());
+  dispatch(clearWishlist());
   persistStore(store).purge(); // Clear persisted state
 };
