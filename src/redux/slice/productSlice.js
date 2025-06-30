49390fb1a2666ba7748b7d798a53a88edc821aef
shafayet <shafayetsec09@gmail.com>
2025-06-30 13:26:36 +0600

    remove best selling slice item from product slice

--- src/redux/slice/productSlice.js
+++ src/redux/slice/productSlice.js
@@ -5,9 +5,6 @@ const initialState = {
     isLoading: false,
     relatedProductsLoading: false,
     items: [],
-    best_selling_Loading: false,
-    best_selling: [],
-    best_selling_error: null,
     flash_sale_Loading: false,
     flash_sale_error: null,
     flash_sale: [],
@@ -38,14 +35,6 @@ export const fetchAllProducts = createAsyncThunk("product/fetchAllProducts", asy
     return {data: response?.data?.data?.results || [], error: response.message};
 });
 
-
-////get Best Selling products
-//export const fetchBestSellingProducts = createAsyncThunk("product/fetchBestSellingProducts", async ({page=1, page_size=null})=>{
-//    let response = await getBestSellingProducts(page, page_size);
-//    console.log('get best selling product res', response);
-    
-//    return {data: response?.data?.data?.results, error: response?.message};
-//});
 
 //get flash sale products
 export const fetchFlashSaleProducts = createAsyncThunk("product/fetchFlashSaleProducts", async ({page=1, page_size=null})=>{
@@ -151,31 +140,6 @@ const productSlice = createSlice({
             //state.products = [];
             state.error = action?.error?.message || 'Something went wrong';
         });
-
-
-        //get bestSelling products
-        //builder.addCase(fetchBestSellingProducts.pending, (state)=>{
-        //    state.relatedProductsLoading = true;
-        //    state.best_selling_Loading = true;
-        //});
-        //builder.addCase(fetchBestSellingProducts.fulfilled,(state, action)=>{
-            
-        //    state.relatedProductsLoading = false;
-        //    state.best_selling_Loading = false;
-        //    state.best_selling_error = null;
-        //    state.best_selling = action.meta.arg.page > 1 
-        //    ? [...state.best_selling, ...action?.payload?.data] 
-        //    : action?.payload?.data;
-        //    state.hasMore = action?.payload?.data?.length === action.meta.arg.limit; // Check if more pages are available
-            
-        //    state.hasMore = action?.payload?.data?.length === action.meta.arg.limit; // Check if more pages are available
-        //});
-        //builder.addCase(fetchBestSellingProducts.rejected,(state, action)=>{
-        //    state.relatedProductsLoading = false;
-        //    state.best_selling_Loading = false;
-        //    //state.products = [];
-        //    state.best_selling_error = action?.error?.message || 'Something went wrong';
-        //});
 
 
         //get flash sale products
